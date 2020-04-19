79b23b9b06c84fc3c663432dc4346734de20d156
yffffff <1012230150@qq.com>
2020-04-20 00:13:41 +0800

    add

---
+++ Week_01/myPow_50.java
@@ -0,0 +1,16 @@
+class myPow_50 {
+    public double myPow(double x, int n) {
+        if (n == 0) { 
+            return 1; 
+        }
+        if (n == 1) {
+            return x; 
+        }
+        if (n == -1) {
+             return 1 / x; 
+        }
+        double half = myPow(x, n / 2);
+        double rest = myPow(x, n % 2);
+        return rest * half * half;
+    }
+}
